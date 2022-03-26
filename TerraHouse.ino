213f9fc934305e256ff624e7809db14485de6815
Pratham Bhat <69955149+PrathamBhatTech@users.noreply.github.com>
2022-03-26 14:30:45 +0530

    Update TerraHouse.ino

--- TerraHouse.ino
+++ TerraHouse.ino
@@ -57,6 +57,81 @@ unsigned long previousMillis = 0;    // will store last time DHT was updated
 // Updates sensor readings every 10 seconds
 const long interval = 10000;  
 
+const char index_html[] PROGMEM = R"rawliteral(
+  <!DOCTYPE HTML><html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <style>
+      html {
+       font-family: Arial;
+       display: inline-block;
+       margin: 0px auto;
+       text-align: center;
+      }
+      h2 { font-size: 3.0rem; }
+      p { font-size: 3.0rem; }
+      .units { font-size: 1.2rem; }
+      .dht-labels{
+        font-size: 1.5rem;
+        vertical-align:middle;
+        padding-bottom: 15px;
+      }
+    </style>
+  </head>
+  <body>
+    <h2>ESP8266 DHT Server</h2>
+    <p>
+      <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+      <span class="dht-labels">Temperature</span> 
+      <span id="temperature">%TEMPERATURE%</span>
+      <sup class="units">&deg;C</sup>
+    </p>
+    <p>
+      <i class="fas fa-tint" style="color:#00add6;"></i> 
+      <span class="dht-labels">Humidity</span>
+      <span id="humidity">%HUMIDITY%</span>
+      <sup class="units">%</sup>
+    </p>
+  </body>
+  <script>
+  setInterval(function ( ) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("temperature").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "/temperature", true);
+    xhttp.send();
+  }, 10000 ) ;
+  
+  setInterval(function ( ) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("humidity").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "/humidity", true);
+    xhttp.send();
+  }, 10000 ) ;
+  </script>
+  </html>)rawliteral";
+  
+
+// Replaces placeholder with DHT values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return String(t);
+  }
+  else if(var == "HUMIDITY"){
+    return String(h);
+  }
+  return String();
+}
+
 void setup()  {
   // Serial port at baud rate 115200 for debugging purposes
   Serial.begin(115200);
@@ -73,6 +148,35 @@ void setup()  {
 
   // Attach the servo on pin 10 to the servo object
   WINDOW.attach(10);
+  
+  dht.begin();
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println(".");
+  }
+
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // TODO: Get data from text menu to put into 
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(t).c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(h).c_str());
+  });
+
+  // Start server
+  server.begin();
   
   // Set optimum conditions for different plants
   // Plant 1 conditions
